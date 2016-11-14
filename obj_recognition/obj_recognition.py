ec79ad7d156cf3d344294a08485f6a8e05051209
svtan9 <selenatan@eduroam-int-dhcp-97-235-61.ucl.ac.uk>
2016-11-14 12:36:14 +0000

    Update: 14/11/2016
    Fixed: Achieved 14% CPU usage with multiprocessing

--- obj_recognition/obj_recognition.py
+++ obj_recognition/obj_recognition.py
@@ -1,3 +1,4 @@
+from multiprocessing import Process, Queue
 import os
 import sys
 import numpy as np
@@ -8,22 +9,38 @@ import mahotas.features
 
 def main(imgPath,bgPath):
 	img = cv2.imread(imgPath)
-	if(os.path.exists(imgPath) == False):
-		print("Foreground image path doesn't exists. Try again.")
 	background = cv2.imread(bgPath)
-	if(os.path.exists(bgPath) == False):
-		print("Background image path doesn't exists. Try again.")
-	img = resizeImg(img)
-	background = resizeImg(background)
-	img = denoise(img)
-	background = denoise(background)
+	queue = Queue()
+	queue2 = Queue()
+	queue3 = Queue()
+	queue4 = Queue()
+	imgP = Process(target=resizeImg, args=(img,queue))
+	bgP = Process(target=resizeImg, args=(background,queue2))
+	imgP.start()
+	bgP.start()
+	img = queue.get()
+	background = queue2.get()
+	imgP.join()
+	bgP.join()
 	obj, mask, cnt = backgroundSubtraction(img,background)
-	colour_set = colourHist(obj, mask)
-	H, area, perimeter, diameter = shapes(obj,cnt)
-	texture = haralick(obj)
-	ORBdes = orbDetect(obj.copy())
+	col = Process(target=colourHist(obj,mask,queue))
+	text = Process(target=haralick, args=(obj,queue2))
+	orb = Process(target=orbDetect, args=(obj.copy(),queue3))
+	kaze = Process(target=orbDetect, args=(obj.copy(),queue4))
+	col.start()
+	text.start()
+	orb.start()
+	kaze.start()
+	colour_set = queue.get()
+	texture = queue2.get()
+	ORBdes = queue3.get()
+	KAZEdes = queue4.get()
+	col.join()
+	text.join()
+	orb.join()
+	kaze.join()
+	H, area, perimeter, diameter = shapes(img,cnt)
 	ORBdes = np.pad(ORBdes,(0,1000-ORBdes.size),'constant',constant_values=0)
-	KAZEdes = kazeDetect(obj.copy())
 	KAZEdes = np.pad(KAZEdes,(0,10000-KAZEdes.size),'constant',constant_values=0)
 	obj_array = appendAll(colour_set,H,area,perimeter,diameter,texture,ORBdes,KAZEdes)
 	print(obj_array.shape)
@@ -34,10 +51,10 @@ def denoise(img):
 	denoised = cv2.fastNlMeansDenoisingColored(img,None,8,10,7,21)
 	return denoised
 
-def resizeImg(img):
+def resizeImg(img,queue):
 	# This method simply resizes the image
 	img = cv2.resize(img, (0,0), fx=0.3, fy=0.3)
-	return img
+	queue.put(img)
 	
 def displayImg(title,img):
 	# This method simply displays the image to the user
@@ -70,14 +87,14 @@ def findCnt(img, mask):
 	displayImg('cropped',img)
 	return img, mask, cnt
 
-def colourHist(img, mask):
+def colourHist(img, mask,queue):
 	color = ('r','g','b')
 	norm_set = []
 	for channel,col in enumerate(color):
 		hist = cv2.calcHist([img],[channel],mask,[32],[0,32])
 		norm = cv2.normalize(hist, hist, 0, 1, cv2.NORM_MINMAX,-1)
 		norm_set.append(norm)
-	return norm_set
+	queue.put(norm_set)
 
 def shapes(img,cnt):
 	# this method computes all sorts of shape descriptors
@@ -88,12 +105,12 @@ def shapes(img,cnt):
 	diameter = radius*2
 	return H, area, perimeter, diameter
 
-def haralick(img):
+def haralick(img,queue):
 	# this method computes the Haralick texture features
 	texture = mahotas.features.haralick(img).mean(0)
-	return texture
+	queue.put(texture)
 
-def orbDetect(img):	
+def orbDetect(img,queue):	
 	# Initiate STAR detector
 	orb = cv2.ORB_create()
 	# find the keypoints with ORB
@@ -101,16 +118,16 @@ def orbDetect(img):
 	# compute the descriptors with ORB
 	kp, des = orb.compute(img, kp)
 	des = np.ravel(np.array(des))
-	return des
+	queue.put(des)
 
-def kazeDetect(img):
+def kazeDetect(img,queue):
 	# load the image and convert it to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # initialize the KAZE descriptor, then detect keypoints and extract local invariant descriptors from the image
     detector = cv2.KAZE_create()
     kp, des = detector.detectAndCompute(gray, None)
     des = np.ravel(np.array(des))
-    return des
+    queue.put(des)
 
 def appendAll(colour_set,H,area,perimeter,diameter,texture,ORBdes, KAZEdes):
 	obj_array = np.ravel(np.array(colour_set))
