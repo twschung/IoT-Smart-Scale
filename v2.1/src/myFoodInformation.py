382b08357cda51e3cadb2c6de7978bef8605561c
svtan9 <selenatan@Selenas-MacBook-Pro.local>
2016-12-07 21:33:04 +0000

    Updated: 7/12/16 (21:32pm)
    Fixed: Added retrieval and saving of food information

--- v2.1/src/myFoodInformation.py
+++ v2.1/src/myFoodInformation.py
@@ -5,49 +5,43 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 import ui_foodinformation
-import db_structure
+import db_access, db_structure
 import time
 
 class myFoodInformation(QWidget, ui_foodinformation.Ui_foodInformation):
 	def __init__(self, mainWindow, currentUserInfo, name=None, layoutSetting=None):
 		super(myFoodInformation, self).__init__()
 		self.setupUi(self)
-		foodInfo = db_structure.foodDataStructure()
+		self.foodInfo = None
 		userDailyIntakeInfo = db_structure.userDailyIntakeStructure()
 		self.btn_new.setText("Scan")
 		if (layoutSetting == "guest"):
 			self.btn_addIntake.setEnabled(False)
 		self.btn_back.clicked.connect(lambda:self.handleBtn_back(mainWindow))
-		self.btn_new.clicked.connect(lambda:self.handleBtn_scan(mainWindow))
+		self.btn_new.clicked.connect(lambda:self.handleBtn_scan(mainWindow,currentUserInfo))
 		self.btn_tare.clicked.connect(lambda:self.handleBtn_tare(mainWindow))
-		self.btn_addIntake.clicked.connect(lambda:self.handleBtn_addIntake(mainWindow))
-	def handleBtn_back(self, mainWindow):
+		self.btn_addIntake.setEnabled(False)
+		self.btn_addIntake.clicked.connect(lambda:self.handleBtn_addIntake())
+	def handleBtn_back(self,mainWindow):
 		mainWindow.central_widget.removeWidget(mainWindow.central_widget.currentWidget())
-	def handleBtn_scan(self, mainWindow):
-		pass
-		'''
+	def handleBtn_scan(self,mainWindow,currentUserInfo):
 		# add camera modules stuff here
-		self.lbl_energyVal.setText(foodInfo.energy)
-		self.lbl_proteinVal.setText(foodInfo.protein)
-		self.lbl_sugarVal.setText(foodInfo.sugars)
-		self.lbl_fibreVal.setText(foodInfo.fibre)
-		self.lbl_fatVal.setText(foodInfo.fat)
-		self.lbl.saltVal.setText(foodInfo.salt)
-		'''
+		self.btn_addIntake.setEnabled(False)
+		self.foodInfo = db_access.food_getActualInfo(currentUserInfo.id,1,100)
+		self.lbl_evergyVal.setText(str(self.foodInfo.energy)) #typo on the ui file, use 'evergy'
+		self.lbl_proteinVal.setText(str(self.foodInfo.protein))
+		self.lbl_sugarVal.setText(str(self.foodInfo.sugars))
+		self.lbl_fibreVal.setText(str(self.foodInfo.fibre))
+		self.lbl_fatVal.setText(str(self.foodInfo.fat))
+		self.lbl_saltVal.setText(str(self.foodInfo.salt))
+		self.btn_addIntake.setEnabled(True)
 	def handleBtn_tare(self, mainWindow):
 		pass
 		# add tare stuff here
-	def handleBtn_addIntake(self, mainWindow):
-		pass
-		'''
-		userDailyIntakeInfo.userid = currentUserInfo.username
-		userDailyIntakeInfo.date = time.strftime("%d/%m/%Y")
-		userDailyIntakeInfo.energy = foodInfo.energy
-		userDailyIntakeInfo.fat = foodInfo.fat
-		userDailyIntakeInfo.sugars = foodInfo.sugars
-		userDailyIntakeInfo.fiber = foodInfo.fibre
-		userDailyIntakeInfo.protein = foodInfo.protein
-		userDailyIntakeInfo.salt = foodInfo.salt
-		'''
+	def handleBtn_addIntake(self):
+		db_access.user_addNewFoodIntake(self.foodInfo)
+		msg = QMessageBox.information(self, 'Added',"Food item has been added to your intake",QMessageBox.Ok)
+		self.btn_addIntake.setEnabled(False)
+
 
 
