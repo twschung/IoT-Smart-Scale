9d667a3a4b8406f890b2e5ee2fe4cfcf33406048
Terry Wai Sing Chung <twschung@gmail.com>
2016-12-08 13:03:42 +0000

    update to myFoodInformation

---
+++ v2.1/ml.py
@@ -0,0 +1,46 @@
+# import cv2
+from sklearn import svm
+from sklearn import tree
+from sklearn.ensemble import BaggingClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.externals import joblib
+from sklearn.decomposition import PCA
+import numpy as np
+import time, os
+import pickle
+import _pickle as cpickle
+
+def loadTrainingSet ():
+	sampleSet = np.load('sampleSet.npy')
+	responseSet = np.load('responseSet.npy')
+	return sampleSet, responseSet
+
+def saveTrainingSet (sampleSet, responseSet):
+	np.save('sampleSet.npy',sampleSet)
+	np.save('responseSet.npy',responseSet)
+
+def addNewDataSet (newSampleSet, newResponseSet):
+	try:
+		sampleSet, responseSet = loadTrainingSet()
+		sampleSet = np.vstack([sampleSet, newSampleSet])
+		responseSet = np.hstack([responseSet,newResponseSet])
+	except:
+		sampleSet = newSampleSet
+		responseSet = newResponseSet
+	saveTrainingSet(sampleSet,responseSet)
+
+class classifier():
+	def __init__(self):
+		self.Model = svm.SVC(C=1.0, kernel='poly', degree=3, probability=True)
+	def load(self):
+		self.Model = joblib.load('Model.dat')
+	def train(self):
+		sampleSet , responseSet = loadTrainingSet()
+		self.Model.fit(sampleSet, responseSet)
+		joblib.dump(self.Model, 'Model.dat')
+	def predict(self, sample):
+		return self.Model.predict(sample)
+	def predict_prob(self, sample):
+		return self.Model.predict_proba(sample)
