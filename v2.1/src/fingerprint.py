f892d8fc5a4aa4e16f2b2dec5c679b89ae70a4f2
Terry Wai Sing Chung <twschung@gmail.com>
2017-01-20 17:13:58 +0000

    --

--- v2.1/src/fingerprint.py
+++ v2.1/src/fingerprint.py
@@ -19,21 +19,21 @@ def connectToFingerPrint():
 def enrollNewFinger(self):
     f = connectToFingerPrint()
     try:
-        msgbox = QDialog()
-        # msgbox.setIcon(QMessageBox.Information)
-        # msgbox.setText("Please place finger on scanner")
-        # msgbox.setWindowModality(Qt.NonModal)
-        msgbox.open();
+        msgbox = QMessageBox()
+        msgbox.setIcon(QMessageBox.Information)
+        msgbox.setText("Please place finger on scanner")
+        msgbox.exec_();
         while ( f.readImage() == False ):
             pass
-        msgbox.done(1)
         f.convertImage(0x01)
         result = f.searchTemplate()
         positionNumber = result[0]
         if ( positionNumber >= 0 ):
+            msgbox.setText("Error")
             return (False, 2)
         else:
             print('Remove finger...')
+            msgbox.setText("Remove finger...")
             time.sleep(2)
             print('Waiting for same finger again...')
 
