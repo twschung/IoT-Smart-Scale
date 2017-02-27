47427a1cb2866ce1e714e3088f8d76c89612d45b
Terry Wai Sing Chung <twschung@gmail.com>
2017-02-27 17:34:48 +0000

    --

---
+++ v3/src/extendedQlabel.py
@@ -0,0 +1,9 @@
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+class ExtendedQLabel(QLabel):
+    def __init__(self, parent):
+        QLabel.__init__(self, parent)
+
+    def mouseReleaseEvent(self, ev):
+        self.emit(SIGNAL('clicked()'))
--- v3/src/myFoodSuggestion.py
+++ v3/src/myFoodSuggestion.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 import ui_foodsuggestionmenu
-import db_access, db_structure
+import db_access, db_structure, extendedQlabel
 currentDir = os.getcwd()
 
 class myFoodSuggestion(QWidget, ui_foodsuggestionmenu.Ui_foodSuggestionMenu):
@@ -13,35 +13,40 @@ class myFoodSuggestion(QWidget, ui_foodsuggestionmenu.Ui_foodSuggestionMenu):
         self.foodWeight = foodWeight
         self.btn_Back.clicked.connect(lambda:mainWindow.central_widget.removeWidget(mainWindow.central_widget.currentWidget()))
         if (len(clfResult_prob)>0):
+            self.lbl_sug1 = extendedQlabel.ExtendedQLabel(self)
             s = str(clfResult_prob[0][0])+".jpg"
             pic = QPixmap(currentDir+'/imageSample/'+s)
             scaledPic = pic.scaled(self.lbl_sug1.width(), self.lbl_sug1.height(),Qt.KeepAspectRatio,transformMode=Qt.SmoothTransformation)
             self.lbl_sug1.setPixmap(scaledPic)
-            self.lbl_sug1.mousePressEvent(lambda:self.handleAddIntake(mainWindow,clfResult_prob[0][0]))
+            self.connect(self.lbl_sug1, SIGNAL('clicked()'), self.handleAddIntake(mainWindow,clfResult_prob[0][0]))
         if (len(clfResult_prob)>1):
+            self.lbl_sug2 = extendedQlabel.ExtendedQLabel(self)
             s = str(clfResult_prob[1][0])+".jpg"
             pic = QPixmap(currentDir+'/imageSample/'+s)
             scaledPic = pic.scaled(self.lbl_sug2.width(), self.lbl_sug2.height(),Qt.KeepAspectRatio,transformMode=Qt.SmoothTransformation)
             self.lbl_sug2.setPixmap(scaledPic)
-            self.lbl_sug2.mousePressEvent(lambda:self.handleAddIntake(mainWindow,clfResult_prob[1][0]))
+            self.connect(self.lbl_sug2, SIGNAL('clicked()'), self.handleAddIntake(mainWindow,clfResult_prob[0][0]))
         if (len(clfResult_prob)>2):
+            self.lbl_sug3 = extendedQlabel.ExtendedQLabel(self)
             s = str(clfResult_prob[2][0])+".jpg"
             pic = QPixmap(currentDir+'/imageSample/'+s)
             scaledPic = pic.scaled(self.lbl_sug3.width(), self.lbl_sug3.height(),Qt.KeepAspectRatio,transformMode=Qt.SmoothTransformation)
             self.lbl_sug3.setPixmap(scaledPic)
-            self.lbl_sug3.mousePressEvent(lambda:self.handleAddIntake(mainWindow,clfResult_prob[2][0]))
+            self.connect(self.lbl_sug3, SIGNAL('clicked()'), self.handleAddIntake(mainWindow,clfResult_prob[0][0]))
         if (len(clfResult_prob)>3):
+            self.lbl_sug4 = extendedQlabel.ExtendedQLabel(self)
             s = str(clfResult_prob[3][0])+".jpg"
             pic = QPixmap(currentDir+'/imageSample/'+s)
             scaledPic = pic.scaled(self.lbl_sug4.width(), self.lbl_sug4.height(),Qt.KeepAspectRatio,transformMode=Qt.SmoothTransformation)
             self.lbl_sug4.setPixmap(scaledPic)
-            self.lbl_sug4.mousePressEvent(lambda:self.handleAddIntake(mainWindow,clfResult_prob[3][0]))
+            self.connect(self.lbl_sug4, SIGNAL('clicked()'), self.handleAddIntake(mainWindow,clfResult_prob[0][0]))
         if (len(clfResult_prob)>4):
+            self.lbl_sug5 = extendedQlabel.ExtendedQLabel(self)
             s = str(clfResult_prob[4][0])+".jpg"
             pic = QPixmap(currentDir+'/imageSample/'+s)
             scaledPic = pic.scaled(self.lbl_sug5.width(), self.lbl_sug5.height(),Qt.KeepAspectRatio,transformMode=Qt.SmoothTransformation)
             self.lbl_sug5.setPixmap(scaledPic)
-            self.lbl_sug5.mousePressEvent(lambda:self.handleAddIntake(mainWindow,clfResult_prob[4][0]))
+            self.connect(self.lbl_sug5, SIGNAL('clicked()'), self.handleAddIntake(mainWindow,clfResult_prob[0][0]))
 
     def handleAddIntake(self,mainWindow,foodID):
         foodInfo = db_access.food_getActualInfo(currentUserInfo.id,str(foodID),str(self.foodWeight))
