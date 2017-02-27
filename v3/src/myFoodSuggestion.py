4142da8db2038839e200a3e2ea1c85bbd5861f1e
Terry Wai Sing Chung <twschung@gmail.com>
2017-02-27 16:07:37 +0000

    --

--- v3/src/myFoodSuggestion.py
+++ v3/src/myFoodSuggestion.py
@@ -4,9 +4,14 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 import ui_foodsuggestionmenu
 import db_access, db_structure
+currentDir = os.getcwd()
 
 class myFoodSuggestion(QWidget, ui_foodsuggestionmenu.Ui_foodSuggestionMenu):
     def __init__(self, mainWindow, currentUserInfo, clfResult_prob):
         super(myFoodSuggestion, self).__init__()
         self.setupUi(self)
-        print (clfResult_prob)
+        self.btn_back.clicked.connect(lambda:mainWindow.central_widget.removeWidget(mainWindow.central_widget.currentWidget()))
+        s = str(clfResult_prob[0][0])+".jpg"
+        pic = QPixmap(currentDir+'/imageSample/'+s)
+        scaledPic = pic.scaled(self.lbl_sug1.width(), self.lbl_sug1.height(),Qt.KeepAspectRatio,transformMode=Qt.SmoothTransformation)
+        self.lbl_sug1.setPixmap(scaledPic)
