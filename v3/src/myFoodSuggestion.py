50d27f9f79a9d8e7882e95e4598415da6783808d
Terry Wai Sing Chung <twschung@gmail.com>
2017-02-27 16:10:38 +0000

    --

--- v3/src/myFoodSuggestion.py
+++ v3/src/myFoodSuggestion.py
@@ -10,7 +10,7 @@ class myFoodSuggestion(QWidget, ui_foodsuggestionmenu.Ui_foodSuggestionMenu):
     def __init__(self, mainWindow, currentUserInfo, clfResult_prob):
         super(myFoodSuggestion, self).__init__()
         self.setupUi(self)
-        self.btn_back.clicked.connect(lambda:mainWindow.central_widget.removeWidget(mainWindow.central_widget.currentWidget()))
+        self.btn_Back.clicked.connect(lambda:mainWindow.central_widget.removeWidget(mainWindow.central_widget.currentWidget()))
         s = str(clfResult_prob[0][0])+".jpg"
         pic = QPixmap(currentDir+'/imageSample/'+s)
         scaledPic = pic.scaled(self.lbl_sug1.width(), self.lbl_sug1.height(),Qt.KeepAspectRatio,transformMode=Qt.SmoothTransformation)
