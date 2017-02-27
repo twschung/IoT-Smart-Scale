9fbe9667b78caa960b3d96ccb30351f684eab447
Terry Wai Sing Chung <twschung@gmail.com>
2017-02-27 15:57:37 +0000

    --

--- v3/src/myFoodSuggestion.py
+++ v3/src/myFoodSuggestion.py
@@ -6,7 +6,7 @@ import ui_foodsuggestionmenu
 import db_access, db_structure
 
 class myFoodSuggestion(QWidget, ui_foodsuggestionmenu.Ui_foodSuggestionMenu):
-	def __init__(self, mainWindow, currentUserInfo, clfResult_prob):
-		super(myFoodSuggestion, self).__init__()
+    def __init__(self, mainWindow, currentUserInfo, clfResult_prob):
+        super(myFoodSuggestion, self).__init__()
         self.setupUi(self)
         print (clfResult_prob)
