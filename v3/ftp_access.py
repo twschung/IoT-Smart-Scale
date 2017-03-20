579c23dc264eb4c4ef853fd0c38e122513b1de2f
Terry Wai Sing Chung <twschung@gmail.com>
2017-03-20 17:06:25 +0000

    --

--- v3/ftp_access.py
+++ v3/ftp_access.py
@@ -65,7 +65,9 @@ def updateImageSample ():
 		finally:
 			urllib.request.urlretrieve ((HTTP_ServerAddress + '/imageSample/imageSample_version.npy'),'imageSample_version.npy')
 			serverImageSampleVersion = np.load('imageSample_version.npy')
+			serverImageSampleVersion = int(serverImageSampleVersion)
 			print (serverImageSampleVersion)
+			print (currentImageSampleVersion)
 			os.remove('imageSample_version.npy')
 			if (serverImageSampleVersion > currentImageSampleVersion):
 				for nextSampleImage in range((currentImageSampleVersion + 1),(serverImageSampleVersion)):
